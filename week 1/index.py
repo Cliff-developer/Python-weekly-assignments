2ae763708c066af48adbd34a233a6188b3765ae1
Cliff-developer <cliffordochieng70@gmail.com>
2024-11-18 14:36:27 +0300

    Assignment submissions

---
+++ week 1/index.py
@@ -0,0 +1,7 @@
+Name = input("What is your name? ")
+Age = input("How old are you? ")
+Location = input("Where are you from? ")
+# Convert Age to an integer
+Age = int(Age)
+# Print the personalized message
+print(f"My name is {Name}, I am {Age} years old and I live in {Location}.")
